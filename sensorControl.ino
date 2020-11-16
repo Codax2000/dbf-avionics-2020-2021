795fecc42d12b4f27329876ec843d28a465beb4b
Codax2000 <aknowlti2000@gmail.com>
2020-11-16 15:23:35 -0800

    added framework for main program

--- sensorControl.ino
+++ sensorControl.ino
@@ -5,10 +5,25 @@
  * Main code for sensor board. Used to control 3 LEDs and read 1 PWM input line.
  */
 
+// import libraries
+// variable for interrupt data
+// variables for servos/relays
+
 void setup()
 {
+  // set output pins
+  // set input pins
+
+  // establish interrupt
+
+  // attach servos/relays
 }
 
 void loop()
 {
+  rc_read_values();
+
+  signal = (int)(rc_input[0]);
+
+  // logic to decide which pin to output on based on PWM signal
 }
